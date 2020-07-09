81ca5021feac99a29c941ba2c517c6871cb5defd
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-07-09 23:09:42 +0200

    add BackDrop and custom button component

---
+++ src/components/UI/BackDrop.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './BackDrop.module.css';
+
+const BackDrop = props =>
+  props.show ? <div className={styles.BackDrop} onClick={props.modalclosed}></div> : null;
+
+export default BackDrop;
