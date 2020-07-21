57005dcb549a6e7e9768152afd7ce30b2161fef3
fgsumer <50924016+fgsumer@users.noreply.github.com>
2020-07-21 18:16:21 +0200

    fixed a small bug

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,7 +4,7 @@ import SideDrawer from '../Navigation/SideDrawer';
 
 class Layout extends Component {
   state = {
-    showSideDrawer: true,
+    showSideDrawer: false,
   };
 
   sideDrawerCloseHandler = () => {
